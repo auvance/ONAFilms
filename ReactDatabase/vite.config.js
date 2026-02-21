6dfc912fda66c80feb47059418aad56dbe073da9
Hafiz Aakif Umar <aakifumar55@gmail.com>
2026-02-21 12:40:49 -0800

    changed app location

--- ReactDatabase/vite.config.js
+++ ReactDatabase/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ONA-Films/',  // ✅ Tells Vite where your app is hosted
-  // ❌ Don't override outDir to 'ONA-Films'
+  base: '/',   // ✅ Vercel
 })
-
