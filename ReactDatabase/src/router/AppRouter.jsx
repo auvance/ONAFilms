271e93b6699452826886ad094743fa87e205eb90
Hafiz Aakif Umar <aakifumar55@gmail.com>
2026-02-21 14:11:15 -0800

    removed browser router directory

--- ReactDatabase/src/router/AppRouter.jsx
+++ ReactDatabase/src/router/AppRouter.jsx
@@ -15,7 +15,7 @@ import PageFavourites from "../pages/PageFav";
 import PageMovie from "../pages/PageMovie";
 import LoginPage from "../pages/Login";
 import PageWelcome from "../pages/Welcome";
-import { APP_FOLDER_NAME } from "../globals/global";
+// import { APP_FOLDER_NAME } from "../globals/global";
 
 const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
@@ -70,7 +70,7 @@ const AppRouter = () => {
 
 const App = () => {
   return (
-    <BrowserRouter basename={`/${APP_FOLDER_NAME}`}>  
+    <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   );
